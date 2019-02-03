e8247341abad3b4c393bdeb3345f148028fdb875
[benfrailey] <[bcfdkp@mail.missouri.edu]>
2019-02-02 19:07:18 -0600

    Added variables and constants

--- Swift Basics/Swift Basics/main.swift
+++ Swift Basics/Swift Basics/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+let sample1: UInt8 = 0x3A
+var sample2: UInt8 = 58
+var heartRate = 85
+var deposits: Double = 135002796
+let acceleration: Float = 9.800
+var mass: Float = 14.6
+var distance = 129.763001
+var lost = true
+var expensive = true
+var choice = 2
+let integral: Character = "\u{222B}"
+let greeting = "Hello"
+var name = "Karen"
+
 
