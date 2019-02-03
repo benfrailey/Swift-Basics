97d8c7e80642437f3bfa4d8514620d25e3091c9d
[benfrailey] <[bcfdkp@mail.missouri.edu]>
2019-02-02 19:29:03 -0600

    Began implementing functions

--- Swift Basics/Swift Basics/main.swift
+++ Swift Basics/Swift Basics/main.swift
@@ -22,4 +22,50 @@ let integral: Character = "\u{222B}"
 let greeting = "Hello"
 var name = "Karen"
 
+if sample1 == sample2 {
+    print("The samples are equal")
+}
+else {
+    print("The samples are not equal")
+}
 
+if heartRate >= 40 && heartRate <= 80 {
+    print("Heart rate is normal")
+}
+else {
+    print("Heart rate is not normal")
+}
+
+if deposits >= 100000000 {
+    print("You are exceedingly wealthy")
+}
+else {
+    print("You are so poor")
+}
+
+var force = mass * acceleration
+
+print("force = ", force)
+print(distance, " is the distance.")
+
+if lost == true {
+    if expensive == true {
+        print("I am really sorry! I will get the manager.")
+    }
+    else{
+        print("Here is coupon for 10% off")
+    }
+}
+
+switch choice {
+case 1:
+    print("You chose 1.")
+case 2:
+    print("You chose 2.")
+case 3:
+    print("You chose 3.")
+default:
+    print("You made an unknown choice.")
+}
+
+print(integral, " is an integral")
