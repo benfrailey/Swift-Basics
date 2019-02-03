a60cb44088f80a4c0869fcbdeb786c0cfbfd637b
[benfrailey] <[bcfdkp@mail.missouri.edu]>
2019-02-02 18:39:31 -0600

    First Commit

---
+++ Swift Basics/Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Ben Frailey on 2/2/19.
+//  Copyright © 2019 Ben Frailey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
